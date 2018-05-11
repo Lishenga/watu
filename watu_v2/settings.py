a98c39ff4c60ba6404c0f44528d24037eb8fa52c
Adams Okode <adams_okode@gmail.com>
2018-05-11 09:08:55 +0300

    UYpdate

--- watu_v2/settings.py
+++ watu_v2/settings.py
@@ -156,7 +156,7 @@ MPESA_SECRET = "sxojQobQfka8f4sv"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 CORS_ORIGIN_ALLOW_ALL = True
